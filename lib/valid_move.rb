c6c175c2e64c284f2bfaa8f64b3414f5bb950c2e
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-19 13:54:58 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@
 # end
 
 def valid_move?(board, index)
-  if index.between?(0..8) && position_taken?(board, index)
+  if !index.between?(0..8) && position_taken?(board, index)
     true
   else
     false
