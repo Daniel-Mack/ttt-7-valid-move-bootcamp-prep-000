537ed7f3de62ef84de176e24ce9c6d65c6308169
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-19 13:42:29 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  !(position_taken?(board, index) && index.between?(1..9)) ? true : false
+  if  index.between?(1..9)
+    if !(position_taken?(board, index)
+      true
+    end
 end
 
 
