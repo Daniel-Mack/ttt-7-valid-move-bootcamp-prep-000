94ae4224b7b39878e3c6d5a98fbaab833ec89851
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-19 13:54:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,7 @@
 # code your #valid_move? method here
-def valid_move?(board, index)
-  !(position_taken?(board, index) && index.between?(0..8)) ? true : false
-end
-
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board, index)
- (board[index] == " " || board[index] == "" || board[index] == nil) ? false : true
-end
+# def valid_move?(board, index)
+#   !(position_taken?(board, index) && index.between?(0..8)) ? true : false
+# end
 
 def valid_move?(board, index)
   if index.between?(0..8)
@@ -16,4 +10,9 @@ def valid_move?(board, index)
     position_taken?(board, index)
     true
   end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+ (board[index] == " " || board[index] == "" || board[index] == nil) ? false : true
 end
